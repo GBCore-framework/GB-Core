977faac8853ae262956f0dfe050d10d1479e7ee7
JulianRadix <julianradix@crowware.nl>
2025-11-03 23:11:38 +0100

    Completed client events

--- events/cl_events.lua
+++ events/cl_events.lua
@@ -20,3 +20,100 @@ GB.Functions.GetKey = function(key)
     return Keys[key]
 end
 
+GB.Functions.GetPlayerData = function(source) -- get all data from database for the player
+    return GB.GetPlayerData
+end
+
+-- Type Admin [ Basic ]
+GB.Functions.DeleteVehicle = function(vehicle)
+    SetEntityAsMissionEntity(vehicle, false, true)
+    DeleteVehicle(vehicle)
+end
+
+GB.Functions.GetVehicleDirection = function()
+    local civilian = PlayerPedId()
+    local civilianCoords = GetEntityCoords(civilian)
+    local inDirection = GetOffsetFromEntityInWorldCoords(civilian, 0, 10, 0)
+    local rayHandle = StartShapeTestRay(civilianCoords, inDirection, 10, civilian, 0)
+    local numRayHandle, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(rayHandle)
+
+    if hit == 1 and GetEntityType(entityType) == 2 then
+        return entityHit
+    end
+
+    return nill
+end
+
+GB.Functions.DeleteObject = function(object)
+    SetEntityAsMissionEntity(object, false, true)
+    DeleteObject(object)
+end
+
+GB.Functions.GetClosestPlayer = function(coords)
+    local players = GB.Functions.GetPlayers()
+    local closestDistance = -1
+    local closestPlayer = -1
+    local coords = coords
+    local usePlayerPed = false
+    local civilian = PlayerPedId()
+    local civilianId = PlayerId()
+
+    if coords == nil then
+        usePlayerPed = true
+        coords = GetEntityCoords(civilian)
+    end
+
+    for i = 1, #players, 1 do
+        local target = GetPlayerPed(players[i])
+
+        if not usePlayerPed or (usePlayerPed and players[i] ~= playerId) then
+            local targetCoords = GetEntityCoords(target)
+            local distance = GetDistanceBetweenCoords(targetCoords, coords.x, coords.y coords.z, true)
+
+            if closestDistance == -1 or closestDistance > distance then
+                closestPlayer = players[i]
+                closestDistance = distance
+            end
+        end
+    end
+    return closestPlayer, closestDistance
+
+end
+
+-- Callbacks
+GB.Functions.TriggerServerCallback = function(name, cb, ...)
+    GB.ServerCallbacks[GB.CurrentRequestId] = cb
+
+    TriggerServerEvent('GB-Core:server:triggerServerCallback', name, GB.CurrentRequestId, ...)
+
+    if GB.CurrentRequestId < 65535 then
+        GB.CurrentRequestId = GB.CurrentRequestId + 1
+    else
+        GB.CurrentRequestId = 0
+    end
+end
+
+GB.Functions.GetPlayers = function()
+    local MaxPlayer = 120
+    local players = {}
+
+    for i=0, MaxPlayer, 1 do
+        local civilian = GetPlayerPed(1)
+        if DoesEntityExist(civilian) then
+            table.insert(players, i)
+        end
+    end
+    return players
+end
+
+RegisterNetEvent('GB-Core:client:serverCallback')
+AddEventHandler('GB-Core:client:serverCallback', function(requestId, ...)
+    GB.ServerCallbacks[requestId](...)
+    GB.ServerCallbacks[requestId] = nil
+end)
+
+-- Other
+RegisterNetEvent('GB-SetCharacterData')
+AddEventHandler('GB-SetCharacterData', function(Player)
+    pData = Player
+end)
